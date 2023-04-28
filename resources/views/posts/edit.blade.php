2e80bf4f4f332c4fa76cb73d715ac81be6c052cc
chitosanny <zhidouz46@gmail.com>
2023-04-28 13:31:34 +0900

    9-3

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,24 +9,30 @@
 
     </head>
     <body class="antialiased">
-        <h1>Blog Name</h1>
-          <form action="/posts/{{ $post->id }}" method="POST">
-              @csrf
-              @method('PUT')
-              <div class="title">
-                  <h2>Title</h2>
-                  <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
-                  <p class ='title__error' style="color:red">{{$errors->first('post.title')}}</p>
-              </div>
-              <div class="body">
-                  <h2>Body</h2>
-                  <textarea name="post[body]" placeholder="今日も一日お疲れ様でした">{{ $post->body }}</textarea>
-                  <p class = 'body__error' style="color:red">{{ $errors->first('post.body')}}</p>
-              </div>
-              <input type ="submit" value="update">
-          </form>  
-            <div class='footer'>
-                <a href="/posts/{{ $post->id }}">戻る</a>
-            </div>
+        <x-app-layout>
+            <x-slot name="header">
+                　Post Edit
+            </x-slot>
+            
+            <h1>Blog Name</h1>
+              <form action="/posts/{{ $post->id }}" method="POST">
+                  @csrf
+                  @method('PUT')
+                  <div class="title">
+                      <h2>Title</h2>
+                      <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
+                      <p class ='title__error' style="color:red">{{$errors->first('post.title')}}</p>
+                  </div>
+                  <div class="body">
+                      <h2>Body</h2>
+                      <textarea name="post[body]" placeholder="今日も一日お疲れ様でした">{{ $post->body }}</textarea>
+                      <p class = 'body__error' style="color:red">{{ $errors->first('post.body')}}</p>
+                  </div>
+                  <input type ="submit" value="update">
+              </form>  
+                <div class='footer'>
+                    <a href="/posts/{{ $post->id }}">戻る</a>
+                </div>
+        </x-app-layout>
     </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,21 +9,27 @@
 
     </head>
     <body class="antialiased">
-        <h1 class ='title'>
-            {{ $post->title }}
-        </h1>
-        <div class='content'>
-                <div class='content_post'>
-                  <h3>本文</h3>
-                  <p class='body'>{{ $post->body }}</p>
-                </div>
-        </div>
-         <div class='edit'>
-            <a href="/posts/{{ $post->id }}/edit">edit</a>
-        </div>
-        <div class='footer'>
-            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-            <a href="/">戻る</a>
-        </div>
+        <x-app-layout>
+            <x-slot name="header">
+                　Post Show
+            </x-slot>
+    
+            <h1 class ='title'>
+                {{ $post->title }}
+            </h1>
+            <div class='content'>
+                    <div class='content_post'>
+                      <h3>本文</h3>
+                      <p class='body'>{{ $post->body }}</p>
+                    </div>
+            </div>
+             <div class='edit'>
+                <a href="/posts/{{ $post->id }}/edit">edit</a>
+            </div>
+            <div class='footer'>
+                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                <a href="/">戻る</a>
+            </div>
+        </x-app-layout>
     </body>
 </html>
